231f55d6e5f21be1754c9bfd23c4b387531f3c53
Kim Levin <klevin5@student.umuc.edu>
2018-02-27 10:42:44 -0500

    initial commit

---
+++ src/MyFirstServlet/MyFirstServlet.java
@@ -0,0 +1,44 @@
+package MyFirstServlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MyFirstServlet
+ */
+@WebServlet("/MyFirstServlet")
+public class MyFirstServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public MyFirstServlet() {
+    	super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String nextURL = "/output.jsp";
+		String message = "This is my message";
+		
+		request.setAttribute("message", message);
+		
+		getServletContext().getRequestDispatcher(nextURL).forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
